5327cdcc90f3b46a4f354891c06943808719b729
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-17 17:22:03 -0600

    refactor: Reorder event methods
    These events are now ordered alphabetically.
    
    This makes it easier to maintain this code.
    
    I also changed some of the usage messages.

--- src/main/java/org/tasker/Event.java
+++ src/main/java/org/tasker/Event.java
@@ -11,21 +11,24 @@ import javafx.scene.input.KeyEvent;
 
 class Event {
   public static final Object[][] keyMap = {
-      { "ENTER", "Change the root of the tree to the selected node" },
       { "ADD", "Zoom in" },
-      { "SUBTRACT", "Zoom out" },
-      { "N", "Add a new node" },
-      { "R", "Rename the selected node" },
-      { "U", "Show usage" },
-      { "S", "Save the tree" },
-      { "Q", "Quit" },
+      { "ENTER", "Change the root of the tree" },
       { "ESCAPE", "Quit" },
-      { "P", "Insert a new node" },
+      { "SPACE", "Go to the root node" },
+      { "SUBTRACT", "Zoom out" },
+      { "TAB", "Show/hide finished nodes" },
+      { "D", "Toggle dark mode" },
       { "H", "Move left" },
       { "J", "Move down" },
       { "K", "Move up" },
       { "L", "Move right" },
-      { "SPACE", "Go to the root node" },
+      { "M", "Mark a node as done" },
+      { "N", "Add a new node" },
+      { "P", "Insert a new node" },
+      { "Q", "Quit" },
+      { "R", "Rename the selected node" },
+      { "S", "Save the tree" },
+      { "U", "Show usage" },
   };
 
   public static void usage() {
@@ -46,71 +49,34 @@ class Event {
     KeyCode code = key.getCode();
 
     switch (code) {
+      case ADD:
+        app.size *= 1.1;
+        app.render();
+        break;
       case ENTER:
         app.tree.current = app.selectedNode;
         app.render();
         break;
-      case ADD:
-        app.size *= 1.1;
+      case SPACE:
+        app.tree.current = app.tree.root;
         app.render();
         break;
       case SUBTRACT:
         app.size *= 0.9;
         app.render();
         break;
-      case N:
-        app.selectedNode.addNode(app);
-        break;
-      case R:
-        app.selectedNode.rename(app);
-        break;
-      case U:
-        usage();
-        break;
-      case S:
-        try {
-          String dateTime = LocalDateTime.now().toString();
-          app.tree.serialize("save.tree", "backups/" + dateTime + ".tree");
-          app.modified = false;
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
+      case TAB:
+        app.showDone = !app.showDone;
         app.render();
         break;
-      case Q:
-      case ESCAPE:
-        if (app.modified) {
-          Alert alert = new Alert(AlertType.CONFIRMATION,
-              "You have unsaved work. Quit anyway?",
-              ButtonType.YES, ButtonType.NO);
-          alert.setTitle("Confirmation Dialog");
-
-          Optional<ButtonType> result = alert.showAndWait();
-          if (result.isPresent() && result.get() == ButtonType.YES) {
-            System.exit(0);
-          }
-        } else {
-          System.exit(0);
-        }
-        break;
-      case P:
-        app.selectedNode = app.selectedNode.insert("new");
-
-        app.modified = true;
+      case D:
+        app.darkMode = !app.darkMode;
         app.render();
         break;
       case H:
         if (app.selectedNode != null) {
           if (app.selectedNode.parent != null) {
             app.selectedNode = app.selectedNode.parent;
-          }
-        }
-        app.render();
-        break;
-      case L:
-        if (app.selectedNode != null) {
-          if (app.selectedNode.children.size() > 0) {
-            app.selectedNode = app.selectedNode.children.get(0);
           }
         }
         app.render();
@@ -137,9 +103,64 @@ class Event {
         }
         app.render();
         break;
-      case SPACE:
-        app.tree.current = app.tree.root;
+      case L:
+        if (app.selectedNode != null) {
+          if (app.selectedNode.children.size() > 0) {
+            app.selectedNode = app.selectedNode.children.get(0);
+          }
+        }
         app.render();
+        break;
+      case M:
+        if (app.selectedNode.attributes.get("status") == null) {
+          app.selectedNode.attributes.put("status", "done");
+        } else if (app.selectedNode.attributes.get("status").equals("done")) {
+          app.selectedNode.attributes.remove("status");
+        }
+
+        app.modified = true;
+        app.render();
+        break;
+      case N:
+        app.selectedNode.addNode(app);
+        break;
+      case P:
+        app.selectedNode = app.selectedNode.insert("new");
+
+        app.modified = true;
+        app.render();
+        break;
+      case R:
+        app.selectedNode.rename(app);
+        break;
+      case S:
+        try {
+          String dateTime = LocalDateTime.now().toString();
+          app.tree.serialize("save.tree", "backups/" + dateTime + ".tree");
+          app.modified = false;
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        app.render();
+        break;
+      case U:
+        usage();
+        break;
+      case Q:
+      case ESCAPE:
+        if (app.modified) {
+          Alert alert = new Alert(AlertType.CONFIRMATION,
+              "You have unsaved work. Quit anyway?",
+              ButtonType.YES, ButtonType.NO);
+          alert.setTitle("Confirmation Dialog");
+
+          Optional<ButtonType> result = alert.showAndWait();
+          if (result.isPresent() && result.get() == ButtonType.YES) {
+            System.exit(0);
+          }
+        } else {
+          System.exit(0);
+        }
         break;
       default:
         System.out.println("Key Pressed: " + key.getCode());
