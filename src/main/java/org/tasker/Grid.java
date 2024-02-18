679fd593dac80a8fd7e12daa5b72cd32b895f4ed
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-18 10:29:44 -0600

    feat: Make the grid go to the edge of the screen
    Previously, the grid was mapped to a finite plane. Now the grid doesn't
    have an end.

--- src/main/java/org/tasker/Grid.java
+++ src/main/java/org/tasker/Grid.java
@@ -4,15 +4,14 @@ import javafx.scene.paint.Color;
 
 class Grid {
   public static void renderGrid(App app, Vec2 span, Vec2 margin, Color c) {
-    Vec2 start = new Vec2(-margin.x, -margin.y);
-    Vec2 end = new Vec2(app.dimensions.x + margin.x, app.dimensions.y + margin.y);
+    app.gc.setStroke(c);
 
-    for (double x = start.x; x <= end.x; x += span.x) {
-      Draw.line(app, new Vec2(x, start.y), new Vec2(x, end.y), c);
+    for (double x = app.globalOffset.x % span.x; x <= app.dimensions.x; x += span.x) {
+      app.gc.strokeLine(x, 0, x, app.dimensions.y);
     }
 
-    for (double y = start.y; y <= end.y; y += span.y) {
-      Draw.line(app, new Vec2(start.x, y), new Vec2(end.x, y), c);
+    for (double y = app.globalOffset.y % span.y; y <= app.dimensions.y; y += span.y) {
+      app.gc.strokeLine(0, y, app.dimensions.x, y);
     }
   }
 }
--- src/main/java/org/tasker/Rect.java
+++ src/main/java/org/tasker/Rect.java
@@ -7,6 +7,10 @@ class Rect {
     return p.x >= x && p.x <= x + w && p.y >= y && p.y <= y + h;
   }
 
+  public boolean intersects(Rect r) {
+    return x < r.x + r.w && x + w > r.x && y < r.y + r.h && y + h > r.y;
+  }
+
   public Rect(double x, double y, double w, double h) {
     this.x = x;
     this.y = y;
