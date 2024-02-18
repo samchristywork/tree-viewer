2944b6407f235c800e1eaa03490f68a8e224ac90
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-02-18 10:49:11 -0600

    feat: Add color schemes
    This allows us to add dark and light color schemes that we can
    dynamically switch between.

--- src/main/java/org/tasker/App.java
+++ src/main/java/org/tasker/App.java
@@ -1,19 +1,41 @@
 package org.tasker;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+class ColorScheme {
+  Color backgroundColor;
+  Color bezierColor;
+  Color borderColor;
+  Color gridColor1;
+  Color gridColor2;
+  Color jumpTextColor;
+  Color nodeBackgroundColor;
+  Color nodeBorderColor;
+  Color nodeCompletedColor;
+  Color nodeHoverColor;
+  Color nodeReparentColor;
+  Color nodeSelectedColor;
+  Color textColor;
+}
+
 public class App extends Application {
+  ColorScheme colorScheme;
+  ColorScheme darkColorScheme = new ColorScheme();
+  ColorScheme lightColorScheme = new ColorScheme();
   GraphicsContext gc;
   Node selectedNode = null;
   Tree tree = new Tree();
@@ -76,28 +98,74 @@ public class App extends Application {
   public void render() {
     gc.clearRect(0, 0, dimensions.x, dimensions.y);
 
-    gc.setFont(javafx.scene.text.Font.font("Arial", 12 * size));
+    // Calculate layout
+    Node n = tree.current;
+    calculateLayout(n);
 
+    // Set color scheme
+    if (darkMode) {
+      colorScheme = darkColorScheme;
+    } else {
+      colorScheme = lightColorScheme;
+    }
+
+    // Render background
+    gc.clearRect(0, 0, dimensions.x, dimensions.y);
+    gc.setFill(colorScheme.backgroundColor);
+    gc.fillRect(0, 0, dimensions.x, dimensions.y);
+
+    // Render grid
     Grid.renderGrid(this, new Vec2(20, 20), new Vec2(100, 100),
-        new Color(0.9, 0.9, 0.9, 1));
+        colorScheme.gridColor1);
     Grid.renderGrid(this, new Vec2(100, 100), new Vec2(100, 100),
-        new Color(0.8, 0.8, 0.8, 1));
+        colorScheme.gridColor2);
 
     Node n = tree.current;
     renderSubtree(n, new Vec2(0, 0));
     lmbClicked = false;
     rmbClicked = false;
 
+    // Render status text
+    gc.setFill(colorScheme.textColor);
+    int fontSize = 16;
+    gc.setFont(Font.font("Arial", fontSize));
+
     if (modified) {
-      int fontSize = 16;
-      gc.setFont(javafx.scene.text.Font.font("Arial", fontSize));
-      gc.setFill(Color.BLACK);
       gc.fillText("modified", 10, 10 + fontSize);
     }
   }
 
   @Override
   public void start(Stage stage) {
+
+    // TODO: Move to a config file
+    darkColorScheme.backgroundColor = Color.BLACK;
+    darkColorScheme.bezierColor = Color.GREY;
+    darkColorScheme.borderColor = new Color(0.5, 0.5, 0.5, 1);
+    darkColorScheme.gridColor1 = new Color(0.1, 0.1, 0.1, 1);
+    darkColorScheme.gridColor2 = new Color(0.2, 0.2, 0.2, 1);
+    darkColorScheme.jumpTextColor = Color.RED;
+    darkColorScheme.nodeBackgroundColor = Color.BLACK;
+    darkColorScheme.nodeBorderColor = Color.GREY;
+    darkColorScheme.nodeCompletedColor = new Color(0.2, 0.4, 0.2, 1);
+    darkColorScheme.nodeHoverColor = new Color(0.2, 0.2, 0.2, 1);
+    darkColorScheme.nodeReparentColor = new Color(0.12, 0.22, 0.32, 1);
+    darkColorScheme.nodeSelectedColor = new Color(0.32, 0.45, 0.5, 1);
+    darkColorScheme.textColor = Color.WHITE;
+    lightColorScheme.backgroundColor = Color.WHITE;
+    lightColorScheme.bezierColor = Color.GREY;
+    lightColorScheme.borderColor = Color.DARKGREY;
+    lightColorScheme.gridColor1 = new Color(0.9, 0.9, 0.9, 1);
+    lightColorScheme.gridColor2 = new Color(0.8, 0.8, 0.8, 1);
+    lightColorScheme.jumpTextColor = Color.DARKRED;
+    lightColorScheme.nodeBackgroundColor = Color.WHITE;
+    lightColorScheme.nodeBorderColor = Color.DARKGREY;
+    lightColorScheme.nodeCompletedColor = new Color(0.6, 0.8, 0.6, 1);
+    lightColorScheme.nodeHoverColor = new Color(0.95, 0.95, 0.95, 1);
+    lightColorScheme.nodeReparentColor = new Color(0.96, 0.88, 0.72, 1);
+    lightColorScheme.nodeSelectedColor = new Color(0.68, 0.85, 0.9, 1);
+    lightColorScheme.textColor = Color.BLACK;
+
     tree.readFromFile("save.tree");
     selectedNode = tree.current;
 
